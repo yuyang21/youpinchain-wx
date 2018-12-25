fec5ae61b70f028e3cfe8dc68d179042da5ad863
yuyang <yuyang@hoolaicom>
2018-12-25 18:26:49 +0800

    fix(confirmGroup):订单确认修改

--- pages/group/confirmGroup/confirmGroup.js
+++ pages/group/confirmGroup/confirmGroup.js
@@ -94,6 +94,146 @@ Page({
         couponId: this.data.coupon.id
       })
     }
+
+    // 最低起售份数
+    this.setData({
+      suitNum: this.data.groupSuit.minimum,
+      showTotal: this.data.productList.length > 2
+    })
+    this.data.groupMyId ? this.getGroupMyAddress() : null;
+    // 运费
+    let that = this;
+    util.request(api.expressCost + this.data.groupSuit.expressCostId).then(res => {
+      let expressCostData = res.data;
+      let fareInfo = '';
+      if (expressCostData.freeExpress === 1) {
+        fareInfo = '(满' + expressCostData.freeExpressValue + '元包邮)'
+      } else if (expressCostData.freeExpress === 2) {
+        fareInfo = '(满' + expressCostData.freeExpressValue + '件包邮)'
+      }
+      that.setData({
+        expressCostData: expressCostData,
+        fareInfo: fareInfo
+      })
+      that.reComputePrice();
+    })
+  },
+  addNumber(e) {
+    let number = e.currentTarget.dataset.number;
+    let suitNum = e.currentTarget.dataset.suitNum;
+    let minimum = this.data.groupSuit.minimum;
+    if (number < 0 && suitNum <= minimum) {
+      if (minimum > 1) {
+        util.showErrorToast('该商品至少购买' + minimum + '份');
+      }
+      return;
+    }
+    suitNum = suitNum + number;
+    this.setData({
+      suitNum: suitNum
+    })
+    this.reComputePrice();
+  },
+  reComputePrice() {
+    this.setData({
+      goodsPrice: 0,
+      totalPrice: 0
+    })
+    let goodsPrice = 0;
+    let packPrice = 0;
+    // 参团
+    if (this.data.groupMy) {
+      packPrice = this.data.groupMy.discountPrice;
+      goodsPrice = goodsPrice + (this.data.groupMy.discountPrice * this.data.suitNum);
+    } else {
+      // 开团根据拼团的类型计算不同的套装价格
+      let that = this
+      if (that.data.groupType === 0) {
+        packPrice = that.data.groupSuit.suitPrice;
+        goodsPrice = goodsPrice + (that.data.groupSuit.suitPrice * that.data.suitNum);
+      } else {
+        that.data.suitTypes.forEach(suitType => {
+          if (suitType.type === that.data.groupSuitType) {
+            if (that.data.groupSuit.id === suitType.productId) {
+              packPrice = suitType.discountPrice;
+              goodsPrice = goodsPrice + (suitType.discountPrice * that.data.suitNum);
+            }
+          }
+        })
+      }
+    }
+    wx.setStorageSync('goodsPrice', JSON.stringify(goodsPrice));
+    let fare = this.data.expressCostData.expressPrice;
+    let expressCostData = this.data.expressCostData;
+    if (expressCostData.freeExpress === 1 && goodsPrice >= expressCostData.freeExpressValue) { // 金额包邮
+      fare = 0;
+    } else if (expressCostData.freeExpress === 2 && this.data.suitNum >= expressCostData.freeExpressValue) { // 数量包邮
+      fare = 0;
+    }
+    this.setData({
+      goodsPrice: goodsPrice,
+      packPrice: packPrice,
+      totalPrice: goodsPrice,
+      fare: fare
+    })
+  },
+  checkAddress(address) {
+    if (!address.name) {
+      util.showErrorToast('请填写收件人姓名')
+      return false;
+    }
+    if (!address.mobile) {
+      util.showErrorToast('请填写收货人手机号')
+      return false;
+    }
+    if (address.mobile.length < 11) {
+      util.showErrorToast('请填写正确的收货人手机号')
+      return false;
+    }
+    if (!address.tipText) {
+      util.showErrorToast('请选择您的所在地区')
+      return false;
+    }
+    if (!address.address) {
+      util.showErrorToast('请填写详细地址')
+      return false;
+    }
+
+    return true;
+  },
+  submitAddress(e) {
+    let address = e.detail.value;
+    if (!this.checkAddress(address)) {
+      return;
+    }
+    util.request(api.addAddress, {
+      name: address.name,
+      provinceId: this.data.provincesId,
+      cityId: this.data.cityId,
+      areaId: this.data.areaId,
+      mobile: address.mobile,
+      address: address.address
+    }, 'POST').then(res => {
+      address.id = res.data;
+      if (res.errno == 0) {
+        wx.setStorageSync('choosedAddress', JSON.stringify(address));
+        this.setData({
+          choosedAddress: JSON.parse(wx.getStorageSync('choosedAddress'))
+        })
+      }
+    });
+  },
+  getGroupMyAddress() {
+    var that = this
+    util.request('/groups/' + that.data.groupSuit.id + '/groupMys/' + that.data.groupMyId).then((res) => {
+      that.setData({
+        groupMy: res.data.groupMy,
+        tuanAddress: res.data.orderAddressVo,
+        groupSuitType: res.data.groupMy.groupSuitType,
+        packPrice: res.data.groupMy.discountPrice
+      })
+      that.reComputePrice();
+    })
   },
   getDefaultAddress () {
     let that = this;
@@ -142,7 +282,7 @@ Page({
     this.setData({
       groupSuitType: event.currentTarget.dataset.suitType
     })
-    // this.reComputePrice();
+    this.reComputePrice();
   },
   changeRegin (e) {
     let arr = e.detail.value;
@@ -171,24 +311,24 @@ Page({
       let provinces = res.data;
       region[0] = that.returnListName(provinces, 'name');
       let provincesId = that.returnListName(provinces, 'id');
-      console.log(region[0])
-      console.log(provincesId)
+      // console.log(region[0])
+      // console.log(provincesId)
       util.request(api.getRegionsList, {
         pid: provinces[0].id
       }).then((res) => {
         let city = res.data;
         let cityId = that.returnListName(city, 'id');
         region[1] = that.returnListName(city, 'name');
-        console.log(region[1])
-        console.log(cityId)
+        // console.log(region[1])
+        // console.log(cityId)
         util.request(api.getRegionsList, {
           pid: city[0].id
         }).then((res) => {
           let area = res.data;
           let areaId = that.returnListName(area, 'id');
           region[2] = that.returnListName(area, 'name');
-          console.log(region[2])
-          console.log(areaId)
+          // console.log(region[2])
+          // console.log(areaId)
           that.setData({
             region: region,
             provinces: region[0],
@@ -200,11 +340,11 @@ Page({
           })
         })
       })
-      console.log(that.data.region)
+      // console.log(that.data.region)
     })
   },
   bindMultiPickerColumnChange (e) {
-    console.log('修改的列为', e.detail.column, '，值为', e.detail.value)
+    // console.log('修改的列为', e.detail.column, '，值为', e.detail.value)
     // this.setData({
     //   region: [[],[],[]]
     // })
@@ -238,8 +378,8 @@ Page({
       case 1:
         var region = [this.data.provinces, this.data.city, []];
         var that = this;
-        console.log(that.data.cityId);
-        console.log(e.detail.value);
+        // console.log(that.data.cityId);
+        // console.log(e.detail.value);
         util.request(api.getRegionsList, {
           pid: that.data.cityId[e.detail.value]
         }).then((res) => {
