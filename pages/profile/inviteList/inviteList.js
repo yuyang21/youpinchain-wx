a1f34135ac8dd84264c09f7da59db670dd9c6a70
yuyang <yuyang@hoolaicom>
2018-12-26 17:02:04 +0800

    fix(inviteList):邀请列表方法添加

--- pages/profile/inviteList/inviteList.js
+++ pages/profile/inviteList/inviteList.js
@@ -49,5 +49,41 @@ Page({
         loading: false
       })
     })
-  }
+  },
+  sharePoster(e) {
+    let that = this;
+    let type = e.currentTarget.dataset.type;
+    if (type === 'close') {
+      that.setData({
+        showPoster: false
+      })
+      return;
+    }
+    //获取我的分享码
+    util.request(api.inviteCode).then(res => {
+      that.setData({
+        url: res.data,
+        showPoster: true
+      })
+      // that.qrcode = new QRCode(document.getElementById('qrcode_1'), {
+      //   text: that.url,
+      //   width: 150,
+      //   height: 150,
+      //   colorDark: '#000000',
+      //   colorLight: '#ffffff'
+      // })
+      if (that.data.finishedDraw) {
+        return
+      }
+      setTimeout(function () {
+        if (that.data.url) {
+          // that.print()
+        }
+      }, 100)
+      that.setData({
+        finishedDraw: true
+      })
+    })
+  },
+  print() {}
 })
